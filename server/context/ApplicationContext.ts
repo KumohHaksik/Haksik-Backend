cdac734a1d148c50836613572804e9bbfeaf9328
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-04-03 20:57:59 +0900

    remove all

--- server/context/ApplicationContext.ts
+++
@@ -1,9 +0,0 @@
-import {DataSource} from "typeorm";
-
-const ApplicationContext = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-}).initialize()
-
-export { ApplicationContext }
